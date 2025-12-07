d4682b7e3a7b546f069674506510bfd9895acc1a
Danny <danny.belmonte4@gmail.com>
2025-12-07 15:51:49 -0600

    Co-authored-by: Abel071895 <Abel071895@users.noreply.github.com>

---
+++ src/components/stage.js
@@ -0,0 +1,19 @@
+import './css/stage.css';
+
+
+function Stage() {
+    return (
+        <>
+            <div className="main-stage">
+                <div className="player-section">
+                    dfdfsf
+                </div>
+                <div className='player-section'>
+                    ffdaf
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Stage;
