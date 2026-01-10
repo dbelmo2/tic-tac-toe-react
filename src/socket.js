1fc9bdb45bb062baf4f392aeb97b32c5a84e21e0
Danny <danny.belmonte4@gmail.com>
2026-01-10 17:54:16 -0600

    fdsfa

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const serverUrl = process.env.SERVER_URL || "http://localhost:3001";
+const serverUrl = process.env.REACT_APP_SERVER_URL || "https://tic-tac-toe-server-production-30dd.up.railway.app";
 
 const socket = io(serverUrl);
 socket.connect();
